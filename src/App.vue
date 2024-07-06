6ca296de73eade847ce3f4905d25e1ba38f9ff21
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-06 21:55:09 +0700

    style: update main app layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import TheHeader from '~/components/TheHeader.vue'
 import TheFooter from '~/components/TheFooter.vue'
+import InputSnippet from '~/components/InputSnippet.vue'
+import OutputSnippet from '~/components/OutputSnippet.vue'
 </script>
 
 <template>
   <TheHeader />
 
-  <main class="flex-1 body-container">
-    App
+  <main class="flex-1">
+    <div class="body-container">
+      <InputSnippet />
+
+      <OutputSnippet />
+    </div>
   </main>
 
   <TheFooter />
