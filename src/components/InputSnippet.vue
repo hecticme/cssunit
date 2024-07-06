814f0da30c091926b12c272bd6263ed8118b6e46
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-06 21:53:05 +0700

    chore: create InputSnippet component

---
+++ src/components/InputSnippet.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputSnippet = ref('')
+</script>
+
+<template>
+  <div class="relative">
+    <textarea
+      id="input-snippet"
+      v-model="inputSnippet"
+      autocapitalize="off"
+      autocomplete="off"
+      autocorrect="off"
+      class="bg-gray-800 px-5 py-3 rounded-lg"
+      name="input-snippet"
+    />
+  </div>
+</template>
