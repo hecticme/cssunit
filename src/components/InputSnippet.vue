807e9813105f7f6624f45a862248ca5e20082452
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-08 15:53:27 +0700

    feat: implement input snippet highlight logic

--- src/components/InputSnippet.vue
+++ src/components/InputSnippet.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { codeToHtml } from 'shiki'
 
 const inputSnippet = ref('')
+const html = ref('')
+
+watch(inputSnippet, async () => {
+  html.value = await codeToHtml(inputSnippet.value, {
+    lang: 'css',
+    theme: 'ayu-dark',
+  })
+})
 </script>
 
 <template>
   <div class="relative">
+    <!-- eslint-disable-next-line -->
+    <div v-html="html"></div>
+
     <textarea
       id="input-snippet"
       v-model="inputSnippet"
