be30288921cf9d0f198297f0759935f23d235bba
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 17:30:24 +0700

    feat(OutputSnippet): implement output code rendering

--- src/components/OutputSnippet.vue
+++ src/components/OutputSnippet.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { codeToHtml } from 'shiki'
+
+const props = defineProps<{
+  code: string
+}>()
+const html = ref('')
+
+watch(() => props.code, async () => {
+  html.value = await codeToHtml(props.code, {
+    lang: 'css',
+    theme: 'ayu-dark',
+  })
+})
+</script>
+
 <template>
-  <div>
-    Output
-  </div>
+  <!-- eslint-disable-next-line -->
+  <div v-html="html"></div>
 </template>
