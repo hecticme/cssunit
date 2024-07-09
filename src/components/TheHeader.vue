026cda0311fbf0f7012f606d68c70d7b8720d8e3
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 23:35:32 +0700

    style(TheHeader): add logo and repository link

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,22 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
 <template>
   <header class="flex items-center h-20">
-    <div class="body-container">
-      header
+    <div class="body-container w-full flex justify-between items-center">
+      <h4 class="font-semibold text-sm italic">
+        CSSUnitConverter
+      </h4>
+
+      <a
+        class="p-2 rounded-lg hover:bg-gray-800 transition-colors"
+        href="https://github.com/hecticme/cssunit"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Icon icon="mingcute:github-line" />
+      </a>
     </div>
   </header>
 </template>
