2e8dbeed6e4ab85de32557992159d6eba6f667e1
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 20:31:56 +0700

    style(OutputSnippet): add copy indicator

--- src/components/OutputSnippet.vue
+++ src/components/OutputSnippet.vue
@@ -45,12 +45,36 @@ function copyOutput () {
   <div class="relative group">
     <button
       aria-label="Copy converted code"
-      class="absolute top-4 right-4 p-2 rounded bg-gray-700 hover:bg-gray-800 border border-gray-600 opacity-20 group-hover:opacity-80 transition-[background-color,opacity] duration-300"
+      class="absolute flex justify-center top-4 right-4 p-2 rounded bg-gray-700 hover:bg-gray-800 border border-gray-600 opacity-20 group-hover:opacity-80 transition-[background-color,opacity] duration-300"
       @click="copyOutput"
     >
+      <Transition>
+        <div
+          v-if="showCopyIndicator"
+          class="absolute -top-[100%] bg-gray-700 border border-gray-600 rounded px-2 py-1 text-xs"
+        >
+          Copied!
+        </div>
+      </Transition>
+
       <Icon icon="ph:copy-duotone" />
     </button>
 
     <div v-html="html" />
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition:
+    opacity 0.25s cubic-bezier(0.4, 0, 0.2, 1),
+    transform 0.25s cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(2px) scale(95%);
+}
+</style>
