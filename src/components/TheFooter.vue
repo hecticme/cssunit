24971f2732b97462d01c93d13a4c7de2161a2c2a
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 23:50:39 +0700

    style(TheFooter): add license

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,14 @@
 <template>
-  <footer class="flex justify-center items-center h-8">
-    footer
+  <footer class="flex justify-center items-center gap-2 h-8 text-sm">
+    <p>2024-PRESENT © H. Ngọc Minh</p>
+
+    <a
+      class="underline underline-offset-4 font-medium hover:text-gray-300 transition-colors duration-500"
+      href="https://opensource.org/license/mit"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      MIT License
+    </a>
   </footer>
 </template>
