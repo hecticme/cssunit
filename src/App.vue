cab1a8eadeaa84fb0b0b0b10428831cae1d1b029
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 17:27:24 +0700

    feat: create a shared state for input code snippet

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import TheHeader from '~/components/TheHeader.vue'
 import TheFooter from '~/components/TheFooter.vue'
 import InputSnippet from '~/components/InputSnippet.vue'
 import OutputSnippet from '~/components/OutputSnippet.vue'
+
+const codeInput = ref('')
 </script>
 
 <template>
@@ -10,9 +13,9 @@ import OutputSnippet from '~/components/OutputSnippet.vue'
 
   <main class="flex-1">
     <div class="body-container grid grid-cols-1 md:grid-cols-2 gap-2">
-      <InputSnippet />
+      <InputSnippet @update-code="userInput => codeInput = userInput" />
 
-      <OutputSnippet />
+      <OutputSnippet :code="codeInput" />
     </div>
   </main>
 
