d0f07b9b2a6bdbfae1c8c942c8e0b40931f0373d
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 20:17:55 +0700

    style(OutputSnippet): add copy-output button

--- src/components/OutputSnippet.vue
+++ src/components/OutputSnippet.vue
@@ -2,6 +2,7 @@
 import { computed, ref, watch } from 'vue'
 import { codeToHtml } from 'shiki'
 import { pxToRem } from '~/utils/pxToRem'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps<{
   code: string
@@ -22,6 +23,14 @@ watch(() => props.code, async () => {
 </script>
 
 <template>
-  <!-- eslint-disable-next-line -->
-  <div v-html="html"></div>
+  <div class="relative group">
+    <button
+      aria-label="Copy converted code"
+      class="absolute top-4 right-4 p-2 rounded bg-gray-700 hover:bg-gray-800 border border-gray-600 opacity-20 group-hover:opacity-80 transition-[background-color,opacity] duration-300"
+    >
+      <Icon icon="ph:copy-duotone" />
+    </button>
+
+    <div v-html="html" />
+  </div>
 </template>
